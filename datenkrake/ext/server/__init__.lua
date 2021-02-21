71c7148ac1e4789178cfb074e48d74113d0833ff
SmR.B <>
2021-02-21 02:30:48 +0100

    Cast bools to string before url concetation
    Lua sucks, Fuck SQLite

--- datenkrake/ext/server/__init__.lua
+++ datenkrake/ext/server/__init__.lua
@@ -1,5 +1,6 @@
 print('Datenkrake is starting ...')
-
+-- v1.2.2 fixed string bool concant (fuck lua)
+-- v1.2.1 Initial Release
 function s (e, v)
 -- Insert your domain:
   local r = 'https://yourdomain.tld/datenkrake.php?event=' .. e .. v
@@ -15,22 +16,22 @@ end)
 
 Events:Subscribe('Server:RoundOver', function(roundTime, winningTeam)
     s('ROUND_END', "&win=" 		.. winningTeam
-    			.. "&players=" 	.. PlayerManager:GetPlayerCount() )
+    			.. "&players=" 	.. tostring(PlayerManager:GetPlayerCount()) )
 end)
 
 -- Player Join / Leave
 Events:Subscribe('Player:Joining', function(name, playerGuid, ipAddress, accountGuid)
     s('PLAYER_JOINED', "&name=" .. name
-                    .. "&pall=" .. PlayerManager:GetPlayerCount()
+                    .. "&pall=" .. tostring(PlayerManager:GetPlayerCount())
                     .. "&ping=0"
                     .. "&guid=0")
 end)
 
 Events:Subscribe('Player:Left', function(player)
-    s('PLAYER_LEFT', "&name=" .. player.name
-    				.. "&pall=" .. PlayerManager:GetPlayerCount()
-    				.. "&ping=" .. player.ping
-    				.. "&points=" .. player.score )
+    s('PLAYER_LEFT', "&name="       .. player.name
+    				.. "&pall="     .. tostring(PlayerManager:GetPlayerCount())
+    				.. "&ping="     .. tostring(player.ping)
+    				.. "&points="   .. tostring(player.score) )
 end)
 
 -- Kill and Supress
@@ -48,20 +49,21 @@ Events:Subscribe('Player:Killed', function(player, inflictor, position, weapon, 
 	end
 
     s('PLAYER_KILLED', "&name=" 		.. player.name
-    				.. "&ping=" 		.. player.ping
+    				.. "&ping=" 		.. tostring(player.ping)
     				.. "&killer=" 		.. killer
-    				.. "&killerping=" 	.. killerping
+    				.. "&killerping=" 	.. tostring(killerping)
     				.. "&weapon=" 		.. weapon
-    				.. "&distance=" 	.. distance
-    				.. "&roadkill=" 	.. isRoadKill
-    				.. "&headshot=" 	.. isHeadShot )
-end)
+    				.. "&distance=" 	.. tostring(distance)
+    				.. "&roadkill=" 	.. tostring(isRoadKill)    -- This is the reason for the frikn 2.7.2 update. UUUhhhh I'm lua, I'm to
+    				.. "&headshot=" 	.. tostring(isHeadShot) )  -- dumb to cast a bool to an string when concenating ... but why does it work with ints?
+end)                                                               -- This language IS aids and yet no one on vu devtalk understands. I want to kill myself.
+                                                                   -- why dont embedd c#? dynamicly compiling with roslyn and embedding mods this way would meke this so easy
 
 Events:Subscribe('Player:SuppressedEnemy', function(player, enemy)
     s('PLAYER_SUPPRESSED', "&name="         .. player.name
-                        .. "&ping="         .. player.ping
+                        .. "&ping="         .. tostring(player.ping)
                         .. "&enemy="        .. enemy.name
-                        .. "&enemyping="    .. enemy.ping)
+                        .. "&enemyping="    .. tostring(enemy.ping))
 end)
 
 
@@ -95,7 +97,7 @@ Events:Subscribe('Player:Resupply', function(player, givenMagsCount, supplier)
     if (supplier ~= nil) then
         s('PLAYER_RESUPPLY',   "&name="     .. player.name
                             .. "&giver="    .. supplier.name
-                            .. "&count="    .. givenMagsCount)
+                            .. "&count="    .. tostring(givenMagsCount) )
     end
 end)
 
@@ -106,5 +108,6 @@ end)
 
 
 
-print('READY: Datenkrake v0.2.1 by SmR.b')
+print('READY: Datenkrake v1.2.2 by SmR.b')
 print('Coded under the influence of weed, alcohol and good music.')
+print('v1.2.2 changes done under the influence of MDMA and some opiods')
